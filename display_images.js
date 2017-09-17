2218cf5fd72a4e16c818ccef1a108c19aeaa229d
Richard Du <rdu@princeton.edu>
2017-09-16 23:12:00 -0400

    Update documentation

--- display_images.js
+++ display_images.js
@@ -3,9 +3,9 @@
 // position : google.maps.LatLng object
 // image_url : string, should be the url to the string
 // other_info : Javascript object, contains all other info that you might need
+// The function will add the images at their respective locations on the map
 function add_image_markers(places, map) {
   places.forEach(function(place) {
-    console.log(place);
     var marker = new google.maps.Marker({
       position: place.position,
       icon: place.image_url,
