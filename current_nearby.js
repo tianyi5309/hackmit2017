465f0eb2ba1883a5b2f3f5a9d81980a5c42b9fff
Richard Du <rdu@princeton.edu>
2017-09-16 23:42:50 -0400

    Unfinished half of pair programming

---
+++ current_nearby.js
@@ -0,0 +1,22 @@
+// Created by Richard (and j o y)
+function get_places(map, api_key) {
+    var northLat = map.getBounds().getNorthEast().lat();
+    var eastLng = map.getBounds().getNorthEast().lng();
+    var southLat = map.getBounds().getSouthWest().lat();
+    var westLng = map.getBounds().getSouthWest().lng();
+
+    var viewport = new google.maps.LatLngBounds(
+        new google.maps.LatLng(southLat, westLng), // SW
+        new google.maps.LatLng(northLat, eastLng)  // NE
+    );
+
+    var request = {
+        location: viewport
+        type: 'point_of_interest'
+    };
+
+    var Httpreq = new XMLHttpRequest(); // a new request
+    Httpreq.open("GET", url, false);
+    Httpreq.send(null);
+    return JSON.parse(Httpreq.responseText);
+}
